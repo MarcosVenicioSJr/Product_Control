7446dc6361abf9c5b0a38d4642574c977d306129
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-06-24 10:53:57 -0300

    feat: Repository criada com seus respectivos métodos

--- src/main/java/com/teste/primeiroexemplo/repository/ProductRepository.java
+++ src/main/java/com/teste/primeiroexemplo/repository/ProductRepository.java
@@ -1,8 +1,48 @@
 package com.teste.primeiroexemplo.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
+import com.teste.primeiroexemplo.model.Product;
+
+import io.micrometer.core.instrument.Meter.Id;
 
 @Repository
 public class ProductRepository {
-  
+  private List<Product> product = new ArrayList<Product>();
+  private Integer lastId = 0;
+
+  public List<Product> GetAll() {
+    return product;
+  }
+
+  public Optional<Product> GetById(Integer id) {
+    return product.stream().filter(product -> product.getId() == id).findFirst();
+  }
+
+  public Product AddProduct(Product model) {
+    lastId++;
+    model.setId(lastId);
+    product.add(model);
+    return model;
+  }
+
+  public void RemoveProduct(Integer id) {
+    product.removeIf(product -> product.getId() == id);
+  }
+
+  public Optional<Product> Update(Product model) {
+    Optional<Product> product = GetById(model.getId());
+
+    product.ifPresent(p -> {
+      p.setName(model.getName());
+      p.setObservation(model.getObservation());
+      p.setQuantity(model.getQuantity());
+      p.setValues(model.getValues());
+    });
+    return product;
+
+  }
 }
