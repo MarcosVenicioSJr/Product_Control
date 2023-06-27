aa4230c8ec655334c6663db05383c7aba8e38a9f
Marcos Venicio <marcos.venicio1230@hotmail.com>
2023-06-27 18:58:05 -0300

    feat: Criando class ErrorMessage

---
+++ src/main/java/com/teste/primeiroexemplo/model/error/ErrorMessage.java
@@ -0,0 +1,38 @@
+package com.teste.primeiroexemplo.model.error;
+
+public class ErrorMessage {
+  private String title;
+  private String message;
+  private Integer status;
+
+  public ErrorMessage(String title, String message, Integer status) {
+    this.title = title;
+    this.message = message;
+    this.status = status;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public Integer getStatus() {
+    return status;
+  }
+
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
+
+}
